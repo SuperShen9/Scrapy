29257841c8e5409e5735999659b33988399bb8d7
super <675153178@qq.com>
2017-04-15 23:50:45 +0800

    简单的测试

---
+++ TEXT.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+list1=['su','2','3']
+list2=['su','11','r']
+for m in list2:
+    print m
+# for i in range(3):
+#     print '123'.replace(list1[i],list2[i])
+# print 'asdfg'.find('d')
