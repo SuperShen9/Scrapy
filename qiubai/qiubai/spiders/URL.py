6302c7ad196f8ab44d07d711ff72bcea9458ac11
super <675153178@qq.com>
2017-04-23 15:52:06 +0800

    qiubai scrapy

---
+++ qiubai/qiubai/spiders/URL.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+import scrapy
+class qiubaispider (scrapy.Spider):
+    name="qiubai"
+    start_urls=[
+        "http://www.qiushibaike.com/",
+    ]
+def parse(self,response):
+    print response.xpath('//div[@class="content"]').extract()
+
