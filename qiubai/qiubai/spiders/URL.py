6f791bbe708e64e17f305eb7a6e228f96cf2322d
super <675153178@qq.com>
2017-04-29 13:38:02 +0800

    20170429 scrapy shell

--- qiubai/qiubai/spiders/URL.py
+++ qiubai/qiubai/spiders/URL.py
@@ -6,5 +6,7 @@ class qiubaispider (scrapy.Spider):
         "http://www.qiushibaike.com/",
     ]
     def parse(self,response):
+        from scrapy.shell import inspect_response
+        inspect_response(response, self)
         print response.xpath('//div[@class="content"]').extract()
 
