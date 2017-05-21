5cfe5bba59b151e02c75c7ea55d99bcf757eff7b
super <675153178@qq.com>
2017-05-21 15:51:56 +0800

    guomin improve

--- text.py
+++ text.py
@@ -1,4 +1,21 @@
 # -*- coding: utf-8 -*-
-def aa(n):
-    for i in range(n):
-        yield i*2
+# def aa(n):
+#     for i in range(n):
+#         yield i*2
+# list1=['a','b','c','d']
+
+# list1.insert(0,'a)
+# list1=['a',2,3,4]
+
+list1=['a']
+for i in list1:
+    d=list(list1)
+    d.remove(i)
+    print d
+
+# list1=['a','b','c','d']
+# for i in list1:
+#     d=list1
+#     d.remove(i)
+#     print d
+
