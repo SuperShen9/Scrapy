032252e8018b575bd4abc4f72f7225ce979432bd
super <675153178@qq.com>
2017-06-11 18:06:34 +0800

    hebin new

--- TTT/hebing_new.py
+++ TTT/hebing_new.py
@@ -1,36 +1,42 @@
 # -*- coding: utf-8 -*-
 import os,openpyxl,xlrd
-os.chdir('D:\zlianxi\hebing_excell')
-file = 'baocun.xlsx'
-if os.path.exists(file):
-    os.remove(file)
 from openpyxl.cell import get_column_letter
-from openpyxl.styles import Font,Style
-spam={'ID':2,'name':3,'age':4}
-k1=0
+from openpyxl.styles import Font
+os.chdir('D:\zlianxi\hebing_excell')
 fath=unicode('D:\zlianxi\hebing_excell','utf-8')
-for foldername,subfolder,excels in os.walk(fath):
-    baocun = openpyxl.Workbook()
-    sheet = baocun.create_sheet(index=0, title='data')
-    for excel in excels:
-        wb=xlrd.open_workbook(excel)
-        sheet1 = wb.sheets()[0]
-        hang = sheet1.nrows
-        lie = sheet1.ncols
-        for k in range(lie):
-            if sheet1.cell(0,k).value in spam.keys():
-                kk = get_column_letter(spam[sheet1.cell(0,k).value])
-                sheet[kk + '1']=sheet1.cell(0,k).value
-                sheet['A1'] = '来源'
-                fontobj = Font(name='Arial', size=12, bold=True)
-                sheet['A1'].font = fontobj
-                sheet[kk + '1'].font = fontobj
-                j = 2
-                for i in range(1,hang):
-                    sheet['A'+str(j+k1)] = excel
-                    sheet[kk+str(j+k1)] =sheet1.cell(i,k).value
-                    j+=1
-        k1+=hang-1
-sheet.freeze_panes='A2'
-baocun.remove_sheet(baocun.get_sheet_by_name('Sheet'))
-baocun.save('baocun.xlsx')
+for x,y,z, in os.walk(fath):
+    for y1 in y:
+        path1='D:\zlianxi\hebing_excell\\'+str(y1)
+        print path1
+        os.chdir(path1)
+        file = 'baocun.xlsx'
+        if os.path.exists(file):
+            os.remove(file)
+        spam={'ID':2,'name':3,'age':4}
+        k1=0
+        fath=unicode(path1,'utf-8')
+        for foldername,subfolder,excels in os.walk(fath):
+            baocun = openpyxl.Workbook()
+            sheet = baocun.create_sheet(index=0, title='data')
+            for excel in excels:
+                wb=xlrd.open_workbook(excel)
+                sheet1 = wb.sheets()[0]
+                hang = sheet1.nrows
+                lie = sheet1.ncols
+                for k in range(lie):
+                    if sheet1.cell(0,k).value in spam.keys():
+                        kk = get_column_letter(spam[sheet1.cell(0,k).value])
+                        sheet[kk + '1']=sheet1.cell(0,k).value
+                        sheet['A1'] = '来源'
+                        fontobj = Font(name='Arial', size=12, bold=True)
+                        sheet['A1'].font = fontobj
+                        sheet[kk + '1'].font = fontobj
+                        j = 2
+                        for i in range(1,hang):
+                            sheet['A'+str(j+k1)] = excel
+                            sheet[kk+str(j+k1)] =sheet1.cell(i,k).value
+                            j+=1
+                k1+=hang-1
+        sheet.freeze_panes='A2'
+        baocun.remove_sheet(baocun.get_sheet_by_name('Sheet'))
+        baocun.save('baocun.xlsx')
--- text11.py
+++ text11.py
@@ -3,14 +3,24 @@ import os,xlrd
 os.chdir('D:\zlianxi\hebing_excell')
 # fath=unicode('E:\movie\美国众神','utf-8')
 fath=unicode('D:\zlianxi\hebing_excell','utf-8')
+fath1=unicode('D:\zlianxi\hebing_excell1','utf-8')
 for x,y,z, in os.walk(fath):
-
-    for y1 in y:
-        print type(y1)
-
+    # print x
+    # print '-' * 50
+    # for y1 in y:
+    #     print y1
+    #     print '+'*20
     for z1 in z:
-        wb = xlrd.open_workbook(z1)
+        print z1
         print type(z1)
+print '-'*50
+for x1, y1, zz, in os.walk(fath):
+    for z1 in zz:
+        print z1
+        print type(z1)
+
+
+
         # print os.path.abspath('.\\'+y1)
 
 
