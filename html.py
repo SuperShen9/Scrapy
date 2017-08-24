a51e2cb7f0d84bdf03a7142e5049e49c0e76d96c
super <675153178@qq.com>
2017-08-25 06:56:24 +0800

    html20170825

---
+++ html.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+# f = open("hello world.html",'w')
+# message = """
+# <html>
+# <head>
+# <meta charset="utf-8">
+# <title>Hello,World!</title>
+# </head>
+# <body>
+# Hello,World!
+# </body>
+# </html>
+# """
+# f.write(message)
+# f.close()
+
+# f = open("version.html",'w')
+# message = """
+# <html>
+# <head>
+# <meta charset="utf-8">
+# <title>标签效果展示</title>
+# </head>
+# <body>
+# <h1>h1:Hello,World!</h1>
+# <h2>h2:Hello,World!</h2>
+# <h3>h3:Hello,World!</h3>
+# <h4>h4:Hello,World!</h4>
+# <h5>h5:Hello,World!</h5>
+# <h6>h6:Hello,World!</h6>
+# <label>label效果展示</label>
+# <span>span效果展示</span>
+# <p>今天又回看了一遍《阿甘正传》，
+# 我第一次知道这部电影是大学课堂上，
+# 我们的物理老师看我们学习态度随意，
+# 对我们认真地说道：建议每个人都回去看看《阿甘正传》。
+# 那天下课之后，我就看了一遍，发现还挺好看的。
+# 之后在大学期间，我回看了几遍。
+# 毕业工作之后，又回看了几遍。尤其是毕业之后的回看，
+# 每次看到的东西都不一样，同样一部电影，
+# 一个人，不同时期，对生活的理解截然不同。</p>
+# <a href="#">a效果展示</a>
+# <a href="https://www.baidu.com/">跳到百度</a>
+# </body>
+# </html>
+# """
+# f.write(message)
+# f.close()
+
+f = open("table.html",'w')
+message = """
+<html>
+<head>
+<meta charset="utf-8">
+<title>table</title>
+</head>
+<body>
+    <table>
+    <thead>
+        <tr>
+            <th>文字类</th>
+            <th>组件类</th>
+            <th>布局类</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>h1</td>
+            <td>form</td>
+            <td>br</td>
+        </tr>
+        <tr>
+            <td>span</td>
+            <td>table</td>
+            <td>div</td>
+        </tr>
+    </tbody>
+</table>
+</body>
+</html>
+"""
+f.write(message)
+f.close()
+
