c4ad5c01490f6a5b43b6628e9fbbf020b1b361ef
super <675153178@qq.com>
2017-09-12 07:29:04 +0800

    20170912

--- TTT.py
+++ TTT.py
@@ -1,19 +1,22 @@
 # -*- coding: utf-8 -*-
 # author:Super
-import re
-a='86-3254888753/456'
-# b=None
-# a=str(b)
-num_Regex=re.compile(r'\d+')
-num_Regex1=re.compile(r'^886-|^86-')
-gz_Regex=re.compile(r'^02')
-mo=num_Regex.findall(a)
-mo1=num_Regex1.sub('0',a)
-mo2=num_Regex.findall(mo1)
-mo3='-'.join(mo2)
-mo4=gz_Regex.sub('02-',mo3)
-print 'mo:' + str(mo)
-print 'mo1:'+ str(mo1)
-print 'mo2'+ str(mo2)
-print '-'.join(mo2)
-print mo4
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+import openpyxl
+baocun = openpyxl.Workbook()
+sheet = baocun.create_sheet(index=0, title='data')
+a=set(u'中国')
+b=set(u'国足')
+s=a|b
+s_out=''.join(s)
+s1=a&b
+
+sheet['A1']=s_out
+# sheet['A2']=b
+# sheet['A3']=s
+
+
+
+
+baocun.save('text.xlsx')
