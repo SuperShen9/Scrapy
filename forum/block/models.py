1740fa515db55e2ddd7e807f0021b8c584256a3f
super <675153178@qq.com>
2017-11-20 21:12:39 +0800

    chenjiejie

--- forum/block/models.py
+++ forum/block/models.py
@@ -6,3 +6,10 @@ class Block(models.Model):
     owner = models.CharField(u'管理员', max_length=100)
     desc = models.CharField(u'板块信息', max_length=100)
 
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name='板块'
+        verbose_name_plural='板块'
+
