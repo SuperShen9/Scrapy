5b5810d15eba00781bce5fab3e0056d16db3f22d
super <675153178@qq.com>
2017-11-22 22:06:28 +0800

    20171122 column

--- forum/views.py
+++ forum/views.py
@@ -5,10 +5,11 @@
 #     return HttpResponse("Hello 驻足五秒")
 
 from django.shortcuts import render
-
 from block.models import Block
+
 def index(request):
-    block_infos = Block.objects.all().order_by('id')
+    # block_infos = Block.objects.all().order_by('id')
+    block_infos = Block.objects.filter(status__gte=0).order_by('id')
     return render(request,"index.html",{'blocks':block_infos})
 
 
