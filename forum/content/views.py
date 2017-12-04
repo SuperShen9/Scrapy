cfffa2422b1c8785fb4ddb080473b494f415d8b4
super <675153178@qq.com>
2017-12-04 23:40:49 +0800

    qingqiu content

--- forum/content/views.py
+++ forum/content/views.py
@@ -1,10 +1,22 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
+from django.shortcuts import redirect
 from block.models import Block
 from content.models import Content
+from article.models import Article
 def content(request,block_id):
-    block_id = int(block_id)
-    block=Block.objects.get(id=block_id)
-    content_objs=Content.objects.filter(block=block,status=0).order_by("id")
-    return render(request,'content.html',{'contents':content_objs,'b':block})
+    if request.method =='GET':
+        block_id = int(block_id)
+        block=Block.objects.get(id=block_id)
+        content_objs=Content.objects.filter(block=block,status=0).order_by("id")
+        return render(request,'content.html',{'contents':content_objs,'b':block})
+
+    else:
+        art_title =request.POST['art_title']
+        art_content=request.POST['art_content']
+        block_id = int(block_id)
+        block = Block.objects.get(id=block_id)
+        article = Article(block=block,title=art_title,content=art_content,status=0)
+        article.save()
+        return redirect('/article/list/%s'%block_id)
 
