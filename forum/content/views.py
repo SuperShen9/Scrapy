625bb116f0d509d3d822bec47524937927dd7331
super <675153178@qq.com>
2017-12-06 23:12:35 +0800

    20171206

--- forum/content/views.py
+++ forum/content/views.py
@@ -2,20 +2,29 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 from block.models import Block
-from content.models import Content
 from article.models import Article
+
 def content(request,block_id):
+    block_id = int(block_id)
+    block = Block.objects.get(id=block_id)
+
     if request.method =='GET':
-        block_id = int(block_id)
-        block=Block.objects.get(id=block_id)
-        content_objs=Content.objects.filter(block=block,status=0).order_by("id")
-        return render(request,'content.html',{'contents':content_objs,'b':block})
+        return render(request,'content.html',{'b':block})
 
     else:
-        art_title =request.POST['art_title']
-        art_content=request.POST['art_content']
-        block_id = int(block_id)
-        block = Block.objects.get(id=block_id)
+        art_title =request.POST['art_title'].strip()
+        art_content=request.POST['art_content'].strip()
+
+        if not art_title or not art_content:
+            return render(request,'content.html',
+                          {'b': block,"error":"标题和内容不能为空",
+                           'art_title':art_title,'art_content':art_content})
+
+        if len(art_title)>100 or len(art_content)>10000:
+            return render(request, 'content.html',
+                          {'b': block, "error": "标题和内容太长",
+                           'art_title': art_title, 'art_content': art_content})
+
         article = Article(block=block,title=art_title,content=art_content,status=0)
         article.save()
         return redirect('/article/list/%s'%block_id)
