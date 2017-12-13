0b4bbd97f67284924cb31e87ef27fd01ee4ee2a8
super <675153178@qq.com>
2017-12-13 22:38:32 +0800

    20171212

--- forum/article/views.py
+++ forum/article/views.py
@@ -2,9 +2,22 @@
 from django.shortcuts import render
 from block.models import Block
 from article.models import Article
+from django.core.paginator import Paginator
 def article_list(request,block_id):
     block_id = int(block_id)
     block=Block.objects.get(id=block_id)
-    article_objs=Article.objects.filter(block=block,status=0).order_by("id")
-    return render(request,'article_list.html',{'articles':article_objs,'b':block})
 
+    cut_page=4
+    page_no = int(request.GET.get("page_no", "1"))
+    all_article=Article.objects.filter(block=block,status=0).order_by("id")
+    p=Paginator(all_article,cut_page)
+    page=p.page(page_no)
+    article_objs=page.object_list
+
+    return render(request,'article_list.html',{'articles':article_objs,'b':block,
+                                               'p':p,'page_no':page_no})
+
+    # page_no=int(request.GET.get("page_no","1"))
+    # start_page=(page_no-1)*cut_page
+    # end_page=page_no*cut_page
+    # [start_page:end_page]
--- forum/detail/views.py
+++ forum/detail/views.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
-from block.models import Block
 from article.models import Article
 
 def Detail(request,article_id):
     article_id=int(article_id)
     article=Article.objects.get(id=article_id)
     return render(request, 'detail.html', {'art': article})
+
