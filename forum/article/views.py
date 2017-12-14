70fb2ed7344410fe9f15ae3f096137451075d86b
super <675153178@qq.com>
2017-12-14 22:44:36 +0800

    20171214

--- forum/article/views.py
+++ forum/article/views.py
@@ -14,8 +14,18 @@ def article_list(request,block_id):
     page=p.page(page_no)
     article_objs=page.object_list
 
+    page_count=p.num_pages
+    page_links=[i for i in range(1, page_count+1) if i>0 and i<=page_count]
+    page_links1 = [i for i in range(page_no - 2, page_no + 3) if i > 0 and i <= page_count]
+    page_small=page_links[0]-1
+    page_max=page_links[-1]+1
+    have_small=page_small>0
+    have_max=page_max<page_count
+
     return render(request,'article_list.html',{'articles':article_objs,'b':block,
-                                               'p':p,'page_no':page_no})
+                                               'p':p,'page_no':page_no,'page_links':page_links,
+                                               'have_small':have_small,'have_max':have_max,
+                                               'page_count':page_count})
 
     # page_no=int(request.GET.get("page_no","1"))
     # start_page=(page_no-1)*cut_page
