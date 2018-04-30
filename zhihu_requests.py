7414cf6f9799c082517a30aa92f17661f2dd6e15
super <675153178@qq.com>
2018-04-30 18:39:28 +0800

    20180430

--- zhihu_requests.py
+++ zhihu_requests.py
@@ -1,15 +1,26 @@
 # -*- coding: utf-8 -*-
-import requests
-from lxml import etree
 
-# url='https://www.zhihu.com/topic/19552832/top-answers'
-# r=requests.get(url).text
-# s=etree.HTML(r)
-# print(s.xpath('//h2[@class="ContentItem-title"]/div/a/text()'))
-# list_py=s.xpath('/html/body/div[1]/div/main/div/div[1]/div[2]/div[3]/div/div/div[2]/div/h2/div/a/text()')
-# for py in list_py:
-#     print(py)
+import requests,time
+import os
+import pandas as pd
 
-print (1)
+headers={
+       'authorization': 'Bearer 2|1:0|10:1525075562|4:z_c0|92:Mi4xY0ZhY0F3QUFBQUFBTU9CNERYbUZEU1lBQUFCZ0FsVk5haHpVV3dBRHBCRXBMbk83OVo4TDk5VHVDTXpmalZBWE9n|65599041233aac4b4a7f994dc624cd1bb2461f47b78cf197aa0f7efec8eb7fe6'
+        ,'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.162 Safari/537.36'
+}
 
-print 2
+follow_data=[]
+def get_follow(page):
+    for i in range(page):
+        url = 'https://www.zhihu.com/api/v4/members/stormzhang/followees?include=data%5B*%5D.answer_count%2Carticles_count%2Cgender%2Cfollower_count%2Cis_f' \
+              'ollowed%2Cis_following%2Cbadge%5B%3F(type%3Dbest_answerer)%5D.topics&offset={}&limit=20'.format(i*20)
+        r=requests.get(url,headers=headers).json()['data']
+        follow_data.extend(r)
+        print('is crawling %s page'%str(i+1))
+        time.sleep(2)
+
+os.chdir('C:\Users\Administrator\Desktop')
+
+get_follow(15)
+df = pd.DataFrame.from_dict(follow_data)
+df.to_excel('follow.xlsx',index=False)
