c582e99f0c59d796964cab5d45a2b6ff81d35a7f
super <675153178@qq.com>
2018-05-04 21:38:19 +0800

    20180504

--- zhuan_Duo.py
+++ zhuan_Duo.py
@@ -9,61 +9,83 @@ df = pd.read_excel('sheet1.xlsx')
 df_PC = pd.read_excel('sheet_PC.xlsx')
 df_MV = pd.read_excel('sheet_MV.xlsx')
 
-# df=pd.merge(left=df, right=df_PC, on=['uuid'],how='right')
-# df=pd.merge(left=df, right=df_MV, on=['uuid'],how='right')
-# print df.info()
-# os.chdir('C:\\Users\Administrator\Desktop')
-# df.to_excel('1.xlsx')
-# exit()
-
 if os.path.exists('RUN'):
     shutil.rmtree('RUN')
 os.makedirs('C:\\Users\Administrator\Desktop\\RUN')
 
 os.chdir('C:\\Users\Administrator\Desktop\\RUN')
 
+for i in range(df.shape[0]):
+    fl = open('%s-%s-%s.txt' % (df['name'].loc[i], df['organization'].loc[i], df['webName'].loc[i]), 'a')
+    for x in df.columns[:-2]:
+        val = df[x].loc[i]
+        if isinstance(val, float):
+            val = ''
+        else:
+            val = val.encode('gbk')
 
+        if x == 'webName':
+            fl.write('{\n')
+            fl.write('\t\r"webUrl": ""\n')
+            fl.write('\t\r"{}\r"'.format(x) + ':' + '\r"' + str(val) + '\r"' + ',')
+            fl.write("\n")
+        else:
+            fl.write('\t\r"{}\r"'.format(x) + ':' + '\r"' + str(val) + '\r"' + ',')
+            fl.write("\n")
+# 合并PC表
+    uuid=df['uuid'].loc[i]
+    fl.write('\t\r"PCinfo": [{')
+    for ii in range(df_PC[df_PC['uuid']==uuid].shape[0]):
+        fl.write("\n")
+        for xx in df_PC.columns:
+            val_pc = df_PC[xx].loc[ii]
+            if  xx == 'time' :
+                val_pc = val_pc
+            else:
+                val_pc = val_pc.encode('gbk')
+            if xx=='uuid':
+                pass
+            else:
+                fl.write('\t\t\r"{}\r"'.format(xx) + ':' + '\r"' + str(val_pc) + '\r"' + ',\n')
+        if ii<df_PC[df_PC['uuid']==uuid].shape[0]-1:
+            fl.write("\t}, {")
+        else:
+            pass
+    fl.write("\t}],")
 
-# for i in range(df.shape[0]):
-#     count=0
-#     for x in df.columns:
-#         count+=1
-#         val = df[x].loc[i]
-#         # if x == 'docUrl' or x == 'remark':
-#         #     val = val
-#         if isinstance(val, float):
-#             val = ''
-#         else:
-#             val = val.encode('gbk')
-#         fl = open('%s-%s-%s.txt' % (df['name'].loc[i],df['organization'].loc[i],df['webName'].loc[i]), 'a')
-#         if count<=len(df.columns)-4:
-#             if x == 'webName':
-#                 fl.write('{\n')
-#                 fl.write('\t\r"webUrl": "http://www.chictr.org.cn/searchproj.aspx"\n')
-#                 fl.write('\t\r"{}\r"'.format(x) + ':' + '\r"' + str(val) + '\r"' + ',')
-#                 fl.write("\n")
-#             elif x == 'remark':
-#                 fl.write('\t\r"remark":"",\n')
-#             else:
-#                 fl.write('\t\r"{}\r"'.format(x) + ':' + '\r"' + str(val) + '\r"' + ',')
-#                 fl.write("\n")
-#         elif count==len(df.columns)-3:
-#             fl.write('\t\r"info": {\n')
-#             fl.write('\t\t\r"name\r"' + ':' + '\r"' + str(val) + '\r"' + ',\n')
-#
-#         elif count==len(df.columns)-1:
-#             if val[-1]=='0':
-#                 fl.write('\t\t\r"{}\r"'.format(x) + ':' + '\r"' + str(val)[:4] + '\r"' + ',\n')
-#             else:
-#                 fl.write('\t\t\r"{}\r"'.format(x) + ':' + '\r"' + str(val)[4:] + '\r"' + ',\n')
-#
-#         elif count == len(df.columns) :
-#             fl.write('\t\t\r"{}\r"'.format(x) + ':' + '\r"' + str(val) + '\r"' + ',\n')
-#             fl.write('\t}\n')
-#             fl.write('}')
-#         else:
-#             fl.write('\t\t\r"{}\r"'.format(x) + ':' + '\r"' + str(val) + '\r"' + ',')
-#             fl.write("\n")
+# 合并MV表
+    fl.write('\n\t\r"MVinfo": [{')
+    for iii in range(df_MV[df_MV['uuid']==uuid].shape[0]):
+        fl.write("\n")
+        for xxx in df_MV.columns:
+            val_mv = df_MV[xxx].loc[iii]
+            if xxx == 'time':
+                val_mv = val_mv
+            else:
+                val_mv = val_mv.encode('gbk')
+            if xxx == 'uuid':
+                pass
+            else:
+                fl.write('\t\t\r"{}\r"'.format(xxx) + ':' + '\r"' + str(val_mv) + '\r"' + ',\n')
+        if iii < df_MV[df_MV['uuid'] == uuid].shape[0] - 1:
+            fl.write("\t}, {")
+        else:
+            pass
+    fl.write("\t}],")
+
+# 补充最后2个字段
+    fl.write('\n\t\r"MDinfo": {')
+    for x in df.columns[-2:]:
+        val = df[x].loc[i]
+        if isinstance(val, float):
+            val = ''
+        else:
+            val = val.encode('gbk')
+        fl.write('\n')
+        fl.write('\t\t\r"{}\r"'.format(x) + ':' + '\r"' + str(val) + '\r"' + ',')
+    fl.write("\n\t}")
+    fl.write("\n}")
+
 
 
 
